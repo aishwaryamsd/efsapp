22f32d16febecbe7cafcac9292d163a2ab7cbc8c
instructor <groyce@unomaha.edu>
2018-02-21 12:08:50 -0600

    Funds

--- efsblog2/settings.py
+++ efsblog2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'e$3s#kktm46=3+#++*cuu6r2ug@_w1$o*n@fxe@t(m$_*sti*h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['aishwaryamsd.pythonanywhere.com']
 
 
 # Application definition
